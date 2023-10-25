d3213bcf9a332e4778d66cff2fc7e6ed32513995
Nguyễn Xuân Trường <97206626+eigthnote72@users.noreply.github.com>
2023-10-25 16:57:07 +0700

    add comment when change status (license , motorbike )

--- src/pages/DashBoardPage/MotorbikeManagement/MotorbikeRegisterDetail.tsx
+++ src/pages/DashBoardPage/MotorbikeManagement/MotorbikeRegisterDetail.tsx
@@ -1,5 +1,5 @@
 
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Typography,TextareaAutosize } from "@mui/material";
 import usei18next from "../../../hooks/usei18next";
 import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
@@ -7,15 +7,32 @@ import MotorbikeManagementService from "../../../services/MotorbikeManagementSer
 import { Motorbike } from "../../../utils/type";
 import MyCustomButton from "../../../components/common/MyButton";
 import ToastComponent from "../../../components/toast/ToastComponent";
+import { useFormik } from "formik";
 
 const MotorbikeRegisterDetail = () => {
     const { t } = usei18next()
     const { id } = useParams();
+    const [statusChange, setStatusChange] = useState<Number>();
     const navigate = useNavigate()
     const [motorbike, setMotorbike] = useState<Motorbike>();
     useEffect(() => {
         getMotobikeById(Number(id))
     }, [id])
+
+    const formik = useFormik({
+        initialValues: {
+            statusComment: ""
+        },
+        onSubmit: values => {
+            changeStatus(Number(statusChange),values.statusComment);
+        }
+    });
+
+    const {
+        values,
+        handleChange,
+        handleSubmit,
+      } = formik;
 
     const getMotobikeById = async (id: number) => {
         try {
@@ -28,13 +45,15 @@ const MotorbikeRegisterDetail = () => {
         }
     }
 
-    const changeStatus = async (status: number) => {
+    const changeStatus = async (status: number, statusComment : string) => {
         try {
-            const response = await MotorbikeManagementService.changeStatus(Number(id),Number(status),"")
+            const response = await MotorbikeManagementService.changeStatus(Number(id),status,statusComment)
             if (response.status === 200) {
                 if(status == 1){
+                    navigate(-1);
                     ToastComponent(t("toast.dashboardMotorbike.Arrprove.success"), "success");
                 }else {
+                    navigate(-1);
                     ToastComponent(t("toast.dashboardMotorbike.Reject.success"), "success");
                 }
                 
@@ -42,7 +61,7 @@ const MotorbikeRegisterDetail = () => {
                 if(status == 1){
                     ToastComponent(t("toast.dashboardMotorbike.Arrprove.warning"), "warning");
                 }else {
-                    ToastComponent(t("toast.dashboardMotorbike.Reject.success"), "success");
+                    ToastComponent(t("toast.dashboardMotorbike.Reject.warning"), "warning");
                 }
             }
         } catch (error) {
@@ -67,6 +86,7 @@ const MotorbikeRegisterDetail = () => {
                 </Box>
             </Box>
             <Box sx={{ display: 'flex', flexDirection: 'column', gap: '3rem', width:'50%' }} >
+            <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', gap: '3rem', width:'100%' }}>
                 <Box sx={{ display: 'flex' }}>
                     <Typography variant="h5" sx={{ color: 'Black', marginRight: 1 }} align="right" fontWeight={'bold'}>{t("dashBoardManager.Motobike.Name")} :</Typography>
                     <Typography variant="h5" align="left">{motorbike?.modelName}</Typography>
@@ -88,10 +108,25 @@ const MotorbikeRegisterDetail = () => {
                     <Typography variant="h5" sx={{ marginTop: 1 }} align="left">{motorbike?.priceRent}</Typography>
                 </Box>
                 <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                    <Button  variant="contained" onClick={() =>  changeStatus(1)}>{t("licenseInfo.BtnApprove")}</Button>
-                    <Button  variant="outlined" onClick={() => changeStatus(2)}>{t("licenseInfo.BtnReject")}</Button>
+                    <Button variant="contained" type="submit" onClick={() =>  setStatusChange(1)}>{t("licenseInfo.BtnApprove")}</Button>
+                    <Button variant="outlined" type="submit" onClick={() => setStatusChange(2)}>{t("licenseInfo.BtnReject")}</Button>
                 </Box>
+                <TextareaAutosize
+                        name="statusComment"
+                        aria-label={t("licenseInfo.InputComment")}
+                        minRows={3}
+                        onChange={handleChange}
+                        value={values.statusComment}
+                        placeholder={t("licenseInfo.InputComment")}
+                        style={{
+                            paddingTop : '10px',
+                            borderRadius : '10px',
+                            fontFamily: 'Arial, sans-serif',
+                            fontSize : '16px'
+                        }}
+                    />
                 <MyCustomButton height="auto" onClick={() => navigate(-1)} content={t("changePassword.Back")} />
+            </form>
             </Box>
         </Box>
     )
